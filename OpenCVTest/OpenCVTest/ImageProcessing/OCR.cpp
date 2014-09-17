e6275a732a61057f0e3d41aed43930bb620fde3e
Muhammad Rashid <m.rashid@coeus-solutions.de>
2014-09-17 19:37:59 +0500

    Added German number plate characters.

--- OpenCVTest/OpenCVTest/ImageProcessing/OCR.cpp
+++ OpenCVTest/OpenCVTest/ImageProcessing/OCR.cpp
@@ -14,6 +14,7 @@
 #include "unicode/utypes.h"
 #include "unicode/utf8.h"
 #include "unicode/uchar.h"
+#include <tuple>
 
 const char OCR::strCharacters[] = {'0','1','2','3','4','5','6','7','8','9','A', 'B', 'C', 'D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '€', '…', '†', '-'};
 const int OCR::numCharacters=30;
