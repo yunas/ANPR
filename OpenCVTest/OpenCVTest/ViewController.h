ac3d507e7e1622e6ff44d08a05149e0107840244
Yunas Qazi <ganifbros@gmail.com>
2014-09-18 16:32:48 +0500

    PhotoBrowser Gallery Added

--- OpenCVTest/OpenCVTest/ViewController.h
+++ OpenCVTest/OpenCVTest/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MWPhotoBrowser.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<MWPhotoBrowserDelegate>
 
 @end
